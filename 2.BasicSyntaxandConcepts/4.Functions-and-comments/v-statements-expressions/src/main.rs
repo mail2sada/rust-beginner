3693de7520216b0d7e2423f9ee280296e444a563
Sadanand Dodawadakar <sadanand.dodawadakar@mavenir.com>
2024-06-20 10:03:24 +0530

    committing code

---
+++ 2.BasicSyntaxandConcepts/4.Functions-and-comments/v-statements-expressions/src/main.rs
@@ -0,0 +1,11 @@
+//demonstraiting statements and expression with example of Multiplication
+fn main() {
+    println!("Demo: Statements and expressions");
+    let x = multiplication(10,20);
+    println!("Valure returned from multiplication is {}", x)
+}
+
+fn multiplication(a:i32, b:i32) ->i32 {
+    println!("Inside function Multiply"); //Statement
+    a * b expression
+}
